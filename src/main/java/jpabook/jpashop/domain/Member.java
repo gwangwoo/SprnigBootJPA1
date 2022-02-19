c39fcc0d02e4101bdeee7af2ed9f3520b7e1be38
gwangwoo <rhkddn5161@naver.com>
2022-02-19 11:05:20 +0900

    스프링부터와 JPA 활용2 - API 개발과 성능 최적화 API 개발 기본 - 회원 등록 API

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import javax.persistence.*;
 
 import javax.persistence.GeneratedValue;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.*;
 
