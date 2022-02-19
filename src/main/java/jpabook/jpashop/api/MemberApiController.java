1e05ae1cb2aa71385c334cdaadba241a45f11e96
gwangwoo <rhkddn5161@naver.com>
2022-02-19 18:08:48 +0900

    스프링부터와 JPA 활용2 - API 개발과 성능 최적화 API 개발 기본 - 회원 조회 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.AllArgsConstructor;
@@ -10,12 +11,20 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
   private final MemberService memberService;
+
+  @GetMapping("/api/v1/members")
+  public List<Member> membersV1() {
+    return memberService.findMembers();
+  }
+
   @PostMapping("/api/v1/members")
   public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
     Long id = memberService.join(member);
@@ -38,11 +47,42 @@ public class MemberApiController {
     return new UpdateMemberResponse(findMember.getId(), findMember.getName());
   }
 
+  @GetMapping("/api/v2/members")
+  public Result memberV2() {
+    List<Member> members = memberService.findMembers();
+    List<MemberDto> collect = members.stream()
+        .map(m -> new MemberDto(m.getName()))
+        .collect(Collectors.toList());
+
+    return new Result(collect.size(), collect);
+  }
+
+
+  @Data
+  @AllArgsConstructor
+  static class ListMemberResponse {
+    private Long id;
+    private String name;
+    Address address;
+  }
 
   @Data
   @AllArgsConstructor
   static class UpdateMemberResponse {
     private Long id;
+    private String name;
+  }
+
+  @Data
+  @AllArgsConstructor
+  static class Result<T> {
+    private int count;
+    private T data;
+  }
+
+  @Data
+  @AllArgsConstructor
+  static class MemberDto {
     private String name;
   }
 
