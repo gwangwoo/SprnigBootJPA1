96bcd8d73ee66e987729a4973b76702ab8905064
gwangwoo <rhkddn5161@naver.com>
2022-02-20 18:35:05 +0900

    스프링부터와 JPA 활용2 - API 개발과 성능 최적화 API 개발 고급 - 지연 로딩과 조회 성능 최적화 간단한 주문 조회 V1: 엔티티를 직접 노출

---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * x to One(ManyToOne, OneToOne)
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+  private final OrderRepository orderRepository;
+
+  @GetMapping("/api/v1/simple-orders")
+  public List<Order> ordersV1() {
+    List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+    all.stream().forEach(o-> o.getMember().getName()); // Lazy 강제 초기화
+    all.stream().forEach(o-> o.getDelivery().getAddress());
+    return all;
+  }
+}
