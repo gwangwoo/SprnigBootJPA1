c1ffbd86c9b1362e4296ca6c409a1d892c382b5c
gwangwoo <rhkddn5161@naver.com>
2022-02-20 19:22:29 +0900

    스프링부터와 JPA 활용2 - API 개발과 성능 최적화 API 개발 고급 - 지연 로딩과 조회 성능 최적화 간단한 주문 조회 V2: 엔티티를 DTO로 변환

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,14 +1,20 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * x to One(ManyToOne, OneToOne)
@@ -29,5 +35,33 @@ public class OrderSimpleApiController {
     all.stream().forEach(o-> o.getMember().getName()); // Lazy 강제 초기화
     all.stream().forEach(o-> o.getDelivery().getAddress());
     return all;
+  }
+
+  @GetMapping("/api/v2/simple-orders")
+  public List<SimpleOrderDto> ordersV2() {
+    // ORDER 2개
+    // N + 1 -> 1 + 회원 N + 배송 N
+    List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+    List<SimpleOrderDto> result = orders.stream()
+        .map(SimpleOrderDto::new)
+        .collect(Collectors.toList());
+    return result;
+  }
+
+  @Data
+  static class SimpleOrderDto {
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+
+    public SimpleOrderDto(Order order) {
+      orderId = order.getId();
+      name = order.getMember().getName();  //LAZY 초기화
+      orderDate = order.getOrderDate();
+      orderStatus = order.getStatus();
+      address = order.getDelivery().getAddress();  //LAZY 초기화
+    }
   }
 }
